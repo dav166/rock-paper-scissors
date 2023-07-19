71d52561f866441ace5ca647965072e04751c4a8
David <5992264+dav166@users.noreply.github.com>
2023-07-19 01:45:16 -0500

    feat: Implement function for generating computer's choice

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 
     //  Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. 
         //  Tip: use the console to make sure this is returning the expected output before moving to the next step!
+const choices = ['Rock', 'Paper', 'Scissors'];
 
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
     //  Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
         //  - Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
