4a90b516438d445cb387fe5741536c65f3d58372
David <5992264+dav166@users.noreply.github.com>
2023-08-09 18:43:37 -0500

    refactor: Update function playToFive() name to game()
    docs: Add descriptive comments to better define code structure

--- game.js
+++ game.js
@@ -1,13 +1,17 @@
+// Define the choices
 const choices = ['Rock', 'Paper', 'Scissors'];
 
+// Function to get the computer's choice
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
+    // Make the player's choice case-insensitive
     playerSelection = playerSelection.toLowerCase();
 
+    // Compare the choices and determine the winner
     if (playerSelection === computerSelection.toLowerCase()) {
         return "It's a draw!";
     } else if (
@@ -25,21 +29,24 @@ const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
             
-function playToFive() {
+function game() {
     let playerScore = 0;
     let computerScore = 0;
 
+    // Play five rounds
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("Rock, paper, or scissors?");
         const computerSelection = getComputerChoice();
         const result = playRound(playerSelection, computerSelection);
 
+        // Update the score based on the result
         if (result.includes("win")) playerScore++;
         else if (result.includes("lose")) computerScore++;
 
         console.log(result);
     }
 
+    // Declare the winner
     if (playerScore > computerScore) {
         console.log("You win the game!");
     } else if (playerScore < computerScore) {
