9d0b291f22bbf02bfd55a50d6ea1794c2da43a65
David <5992264+dav166@users.noreply.github.com>
2023-08-10 04:15:43 -0500

    refactor: Improve code structure, accessibility, and styles for better UX

--- game.js
+++ game.js
@@ -1,21 +1,23 @@
-// Constants and Variables
+// --- Constants and Initialization --- //
 const choices = ['Rock', 'Paper', 'Scissors'];
 let targetScore = 0; 
 let playerScore = 0;
 let computerScore = 0;
 
-// Utility functions
+// --- Utility Functions --- //
 const capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 
-// Game functions
 const getComputerChoice = () => choices[Math.floor(Math.random() * 3)];
 
-const playRound = (playerSelection) => {
+const setTargetScore = gameType => gameType === 3 ? 3:5;
+
+// --- Core Game Logic --- //
+const playRound = playerSelection => {
     const computerSelection = getComputerChoice();
-    // Make the player's choice case-insensitive
     playerSelection = playerSelection.toLowerCase();
 
     let result;
+
     // Compare the choices and determine the winner
     if (playerSelection === computerSelection.toLowerCase()) {
         result = "It's a draw!";
@@ -24,91 +26,55 @@ const playRound = (playerSelection) => {
         (playerSelection === "scissors" && computerSelection === "Paper") ||
         (playerSelection === "paper" && computerSelection === "Rock")
     ) {
-        result = "You win! " + capitalizeFirstLetter(playerSelection) + " beats " + computerSelection.toLowerCase() + ".";
+        result = `You win! $(capitalizeFirstLetter(playerSelection)) beats $(computerSelection.toLowerCase()).`;
         updateScore('player');
     } else {
-        result = "You lose! " + capitalizeFirstLetter(computerSelection) + " beats " + playerSelection + ".";
+        result = `You lose! $(capitalizeFirstLetter(computerSelection)) beats $(playerSelection)`;
         updateScore('computer');
     }
     document.getElementById("results").textContent = result;
 };
 
-const updateScore = (winner) => {
-    if (winner === 'player') {
-        playerScore++;
-    } else if (winner === 'computer') {
-        computerScore++;
-    }
-    document.getElementById("score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+const updateScore = winner => {
+    winner === 'player' ? playerScore++ : computerScore++;
 
-    // Check if either player or computer has reached the target score
+    document.getElementById("score").textContent = `Player: $(playerScore) | Computer: $(computerScore)`;
+
     if (playerScore === targetScore || computerScore === targetScore) {
-        const winner = playerScore === targetScore ? "Congratulations! You win" : "Oh no! The computer wins";
-        document.getElementById("results").textContent = `${winner} the game!`;
+        document.getElementById("results").textContent = playerScore === targetScore ? "Congratulations! You win the game!" : "Oh no! The computer wins!";
         resetGame();
     }
-    console.log("Updating score. Player:", playerScore, "Computer:", computerScore);
 };
 
-const startGame = (gameType) => {
-    // Reset scores
-    playerScore = 0;
-    computerScore = 0;
-    document.getElementById("score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+const startGame = gameType => {
+    [playerScore, computerScore, targetScore] = [0, 0, setTargetScore(gameType)];
 
+    document.getElementById("score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
     document.getElementById("startScreen").style.display = "none";
     document.getElementById("gameScreen").style.display = "block";
     document.getElementById("gameType").textContent = `First to ${gameType}`;
-
-    // Set the target score based on game type
-    if (gameType === 3) {
-        targetScore = 3;
-    } else if (gameType === 5) {
-        targetScore = 5;
-    }
-    console.log("Game started with target score:", targetScore);
 };
 
 const resetGame = () => {
-    // Update the displayed scores
-    document.getElementById("score").textContent = `Final Score - Player: ${playerScore} | Computer: ${computerScore}`;
+    const postGameContainer = document.getElementById("postGameContainer");
+    postGameContainer.innerHTML = '';
 
-    // Hide the main game screen
-    document.getElementById("gameScreen").style.display = "none";
-
-    // Show the start screen with post-game options
-    document.getElementById("startScreen").style.display = "block";
-
-    let postGameContainer = document.getElementById("postGameContainer");
-
-    if (!postGameContainer) {
-        postGameContainer = document.createElement("div");
-        postGameContainer.id = "postGameContainer";
-        document.getElementById("startScreen").appendChild(postGameContainer)
-    } else {
-        postGameContainer.innerHTML = ''; // Clear previous content
-    }
-
-    let postGameMessage= document.createElement("p");
-    if (playerScore > computerScore) {
-        postGameMessage.textContent = "Congratulations! You won!";
-    } else {
-        postGameMessage.textContent = "Better luck next time!";
-    }
+    let postGameMessage = document.createElement("p");
+    postGameMessage.textContent = playerScore > computerScore ? "Congratulations! You won!" : "Better luck next time!";
     postGameContainer.appendChild(postGameMessage);
 
-    let playAgainButton = document.createElement("button");
+    const playAgainButton = document.createElement("button");
     playAgainButton.textContent = "Play Again";
     playAgainButton.addEventListener("click", function() {
         playerScore = 0;
         computerScore = 0;
         targetScore = 0;
-        postGameContainer.remove(); // Remove the post-game container
+        postGameContainer.remove();
         document.getElementById("score").textContent = `Player: ${playerScore} | Computer: $(computerScore)`;
     });
     postGameContainer.appendChild(playAgainButton);
 
-    let quitButton = document.createElement("button");
+    const quitButton = document.createElement("button");
     quitButton.textContent = "Quit";
     quitButton.addEventListener("click", function() {
         window.close();
@@ -116,16 +82,20 @@ const resetGame = () => {
     postGameContainer.appendChild(quitButton);
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    const rockButton = document.getElementById("rock");
-    const paperButton = document.getElementById("paper");
-    const scissorsButton = document.getElementById("scissors");
-    const gameTypeButtons = document.querySelectorAll("[data-game-type");
+const createPostGameOptions = () => {
+    const postGameContainer = document.createElement("div");
+    postGameContainer.id = "postGameContainer";
+    document.getElementById("startScreen").appendChild(postGameContainer);
+    return postGameContainer;
+};
 
-    rockButton.addEventListener("click", () => playRound("rock"));
-    paperButton.addEventListener("click", () => playRound("paper"));
-    scissorsButton.addEventListener("click", () => playRound("scissors"));
-    gameTypeButtons.forEach(button => {
+// --- Event Listeners --- //
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("rock").addEventListener("click", () => playRound("rock"));
+    document.getElementById("paper").addEventListener("click", () => playRound("paper"));
+    document.getElementById("scissors").addEventListener("click", () => playRound("scissors"));
+
+    document.querySelectorAll("[data-game-type").forEach(button => {
         button.addEventListener("click", function() {
             const gameType = parseInt(this.getAttribute("data-game-type"));
             startGame(gameType);
