ec5b6ead9a716f5d3fdcb7aaf6897319fda581d8
David <5992264+dav166@users.noreply.github.com>
2023-08-09 22:39:00 -0500

    fix: Improve game reset function and address score-keeping issue

--- game.js
+++ game.js
@@ -2,36 +2,23 @@ let targetScore = 0; // Set default values
 let playerScore = 0;
 let computerScore = 0;
 
-function startGame(selectedTargetScore) {
-    document.getElementById("startScreen").style.display = "none";
-    document.getElementById("gameScreen").style.display = "block";
-    document.getElementById("gameType").textContent = `First to ${selectedTargetScore}`;
-    targetScore = selectedTargetScore
-}
-
-function resetGame() {
+function startGame(gameType) {
+    // Reset scores
     playerScore = 0;
     computerScore = 0;
-
-    // Update the displayed scores
     document.getElementById("score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 
-    // Hide the main game screen
-    document.getElementById("gameScreen").style.display = "none";
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "block";
+    document.getElementById("gameType").textContent = `First to ${gameType}`;
 
-    // Show the start screen
-    document.getElementById("startScreen").style.display = "block";
-
-    let postGameMessage= document.createElement("p");
-    postGameMessage.textContent = "Would you like to play again? Choose a game type or quit.";
-    document.getElementById("startScreen").appendChild(postGameMessage);
-
-    let quitButton = document.createElement("button");
-    quitButton.textContent = "Quit";
-    quitButton.addEventListener("click", function() {
-        window.close();
-    });
-    document.getElementById("startScreen").appendChild(quitButton);
+    // Set the target score based on game type
+    if (gameType === 3) {
+        targetScore = 3;
+    } else if (gameType === 5) {
+        targetScore = 5;
+    }
+    console.log("Game started with target score:", targetScore);
 }
 
 // Define the choices
@@ -83,15 +70,54 @@ function updateScore(winner) {
         document.getElementById("results").textContent = `${winner} the game!`;
         resetGame();
     }
+    console.log("Updating score. Player:", playerScore, "Computer:", computerScore);
 }
 
-// Add event listeners to buttons
-document.getElementById("rock").addEventListener("click", function() {
-    playRound("rock");
+document.addEventListener("DOMContentLoaded", function() {
+    // Add event listeners to buttons
+    document.getElementById("rock").addEventListener("click", function() {
+        playRound("rock");
+    });
+    document.getElementById("paper").addEventListener("click", function() {
+        playRound("paper");
+    });
+    document.getElementById("scissors").addEventListener("click", function() {
+        playRound("scissors");
+    });
 });
-document.getElementById("paper").addEventListener("click", function() {
-    playRound("paper");
-});
-document.getElementById("scissors").addEventListener("click", function() {
-    playRound("scissors");
-});
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    targetScore = 0;
+
+    // Update the displayed scores
+    document.getElementById("score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+
+    // Hide the main game screen
+    document.getElementById("gameScreen").style.display = "none";
+
+    // Show the start screen
+    document.getElementById("startScreen").style.display = "block";
+
+    let postGameContainer = document.getElementById("postGameContainer");
+
+    if (!postGameContainer) {
+        postGameContainer = document.createElement("div");
+        postGameContainer.id = "postGameContainer";
+        document.getElementById("startScreen").appendChild(postGameContainer)
+    } else {
+        postGameContainer.innerHTML = ''; // Clear previous content
+    }
+
+    let postGameMessage= document.createElement("p");
+    postGameMessage.textContent = "Would you like to play again? Choose a game type or quit.";
+    postGameContainer.appendChild(postGameMessage);
+
+    let quitButton = document.createElement("button");
+    quitButton.textContent = "Quit";
+    quitButton.addEventListener("click", function() {
+        window.close();
+    });
+    postGameContainer.appendChild(quitButton);
+}
